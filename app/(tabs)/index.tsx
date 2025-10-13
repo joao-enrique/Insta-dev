f1cad6f8fb1123e6e0555fea6deffa0aeac0777f
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-10-13 10:34:09 -0300

    💾 Feat: Fetching Posts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
+import { Loader } from "@/components/Loader";
+import Post from "@/components/Post";
+import Story from "@/components/Story";
+import { STORIES } from "@/constants/mock-data";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
 import { useAuth } from "@clerk/clerk-expo";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
-import { styles } from "../../styles/auth.styles";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { styles } from "../../styles/feed.styles";
 
 export default function Index() {
   const { signOut } = useAuth();
   const router = useRouter();
+  const posts = useQuery(api.posts.getFeedPosts)
+
+  if(posts === undefined) return <Loader />
+
+  if(posts.length === 0) return <NoPostsFound />
 
   const handleSignOut = async () => {
     try {
@@ -18,9 +31,47 @@ export default function Index() {
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleSignOut}>
-        <Text style={{ color: "white" }}>Sign out</Text>
-      </TouchableOpacity>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Insta Dev</Text>
+        <TouchableOpacity onPress={() => handleSignOut()}>
+          <Ionicons name="log-out-outline" size={24} color={COLORS.white} />
+        </TouchableOpacity>
+      </View>
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{paddingBottom: 60}}
+      >
+        {/* STORIES */}
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (
+          <Story key={story.id} story={story}/>
+        ))}
+        </ScrollView>
+
+        {posts?.map((post) => (
+          <Post key={post._id} post={post}/>
+        ))}
+      </ScrollView>
+
     </View>
   );
 }
+
+const NoPostsFound = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: COLORS.background,
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ fontSize: 20, color: COLORS.primary }}>No posts yet</Text>
+  </View>
+);
